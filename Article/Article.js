7c0900541a96048d2d69c9ca05562fb76bc787c9
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-04 17:40:16 -0800

    finished menu

--- Article/Article.js
+++ Article/Article.js
@@ -112,6 +112,7 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
 function createArticle (content) {
   //define new elements
   const article = document.createElement('div');
